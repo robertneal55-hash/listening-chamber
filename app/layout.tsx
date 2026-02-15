06fa67cab9c19abcaf3106d59c250e6fa07e0d77
robertneal55-hash <robertneal55@gmail.com>
2026-02-15 14:09:52 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-
+import {Providers} from ',/providers'
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -30,8 +30,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+  <html lang="en">
+    <body className={inter.className}>
+      <Providers>
+        {children}
+      </Providers>
+    </body>
+  </html>
   );
 }
